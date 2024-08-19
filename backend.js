c5a96deff28f74c293fbfebd890adacc6d14a099
Chiem <c.p.stevens@student.tue.nl>
2024-08-19 17:30:47 +0200

    no more undefined

--- backend.js
+++ backend.js
@@ -9,8 +9,10 @@ let runners = new Map();
 app.get("/", (req, res) => {
   const { user, state, scope, code } = req.query;
   console.log(`User: ${user}, State: ${state}, Scope: ${scope}, Code: ${code}`);
-  runners.set(user, code);
-  res.send(`Code: ${code}`);
+  if (user!=undefined && code!=undefined) {
+    runners.set(user, code);
+    res.send(`Code: ${code}`);
+  }
 });
 
 app.get("/runners", (req, res) => {
